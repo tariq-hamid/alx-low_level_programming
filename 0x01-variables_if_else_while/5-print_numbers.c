3d77681832201ade510b99202f3d6c11bb5fe5ea
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-27 11:49:38 -0500

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	printf(i);
+}
+printf("\n");
+return (0);
+}
+
