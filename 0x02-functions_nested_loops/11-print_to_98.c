d86f24c18eedb6e6735d2ac5ece9849317a47817
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 09:04:32 -0500

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: passed to print_to_98
+ *
+ * Return: nth, void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		printf("%d,", n);
+		n++;
+		while (n < 98)
+		{
+			printf(" %d,", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		printf("%d,", n);
+		n--;
+		while (n < 98)
+		{
+			printf(" %d,", n);
+			n--;
+		}
+	}
+	else
+		printf("%d", n);
+}
+
