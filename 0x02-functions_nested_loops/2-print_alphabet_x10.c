edfb88046d3bd5c60f67bfa73eae2c0e6c388104
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 04:45:13 -0500

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10x to the stdout
+ *
+ * Return - returns nth, void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(i);
+		}
+		_putchar(10);
+	}
+}
+
