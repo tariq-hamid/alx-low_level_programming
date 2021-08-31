3af415b5915b5e9d709a20e4c6867ef4487b4f5b
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 02:35:12 -0500

    Task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - check the code for ALX School students.
+ * main - Entry point, prints "Holberton".
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	prinf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
 	return (0);
 }
