06bad6730b28164bd774152a09ee8292a00e5c18
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 06:25:06 -0500

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+/**
+ * print_last_digit - returns the last digit of n
+ * @n: passed to print_last_digit
+ *
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
