bf7e01a6dad639c4792c0cd539c0e147dc26e285
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 13:52:57 -0500

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long sum, x, y;
+	int i;
+
+	i = 0;
+	sum = 0;
+	x = 1;
+	y = 2;
+	while (y < 4000000)
+	{
+		sum = y + x;
+		if (i == 0)
+			printf("%ld, %ld, ", x, y);
+		else if (i == 48)
+			printf("%ld\n", y);
+		else
+			printf("%ld, ", y);
+		x = y;
+		y = sum;
+		i++;
+	}
+	return (0);
+}
+
