500a02d71ca947f9cf4f1b3ad57dfe700721c657
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 09:40:31 -0500

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+# include<stdio.h>
 
 /**
  * print_times_table - prints the n times table
@@ -8,45 +8,18 @@
  */
 void print_times_table(int n)
 {
-	int i, j, ij, k;
+	int i, j;
 
-	for (j = 0; j <= n; j++)
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			ij = j * i;
-			if (i != 0)
-			{
-				_putchar(',');
-				for (k = len(ij); k <= len(n * n); k++)
-					_putchar('  ');
-			}
-			if (ij > 9)
-			{
-				_putchar(ij / 10 + '0');
-			}
-			_putchar(ij % 10 + '0');
+			if (j == 0)
+				printf("0");
+			else
+				printf("%4d", i * j);
 		}
-		_putchar('\n');
+		printf("\n");
 	}
 }
 
-/**
- * len - counts the number of digits in a number
- * @n: number to be used
- *
- * Return: returns the number of digits
- */
-int len(int n)
-{
-	int digits = 0;
-
-	if (n == 0)
-		return (1);
-	while (n != 0)
-	{
-		n /= 10;
-		digits++;
-	}
-	return (digits);
-}
