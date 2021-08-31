56c10ab58df89f7806fc3b849f73d1874acc939d
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 13:12:52 -0500

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum, x;
+
+	i = 0;
+	sum = 0;
+	x = 0;
+	while (i < 50)
+	{
+		sum = sum + x;
+		if (i == 49)
+			printf("%d", x);
+		else
+			printf("%d, ", x);
+		x = sum;
+		i++;
+	}
+}
+
