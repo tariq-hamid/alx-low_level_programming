7ec5ffbafb04aad86c6b64a6e79e1f307b2ccdcf
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 14:00:45 -0500

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,21 +7,23 @@
  */
 int main(void)
 {
-	long sum, x, y;
-	int i;
+	long sum, x, y, sum_;
 
-	i = 0;
 	sum = 0;
+	sum_ = 0;
 	x = 1;
 	y = 2;
 	while (y < 4000000)
 	{
+		if (x % 2 == 0)
+			sum_ = sum_ + x;
+		if (y % 2 == 0)
+			sum_ = sum_ + y;
 		sum = y + x;
 		x = y;
 		y = sum;
-		i++;
 	}
-	printf("%ld\n", sum);
+	printf("%ld\n", sum_);
 	return (0);
 }
 
