1feb1822efc21d89b084c564e44a57fda0324ffe
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 13:02:18 -0500

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum = sum + i;
+		else
+			continue;
+	}
+	printf("%d", sum);
+}
+
