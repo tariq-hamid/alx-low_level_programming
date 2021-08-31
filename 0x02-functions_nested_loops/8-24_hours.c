1ce35e5e0a208693a8a6fe121dd1d45f38dea319
Tariq Hamid <tariqhamid416@gmail.com>
2021-08-31 06:46:56 -0500

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ *
+ * Return: nth, void
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i + '0');
+			_putchar(':');
+			if (j < 10)
+				_putchar('0');
+			_putchar(j + '0');
+		}
+	}
+}
+
