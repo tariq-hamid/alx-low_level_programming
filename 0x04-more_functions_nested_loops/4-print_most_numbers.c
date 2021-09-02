5b919df88b6ff537e45e8fd901f68dd799472452
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 10:13:08 -0500

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 to stdout, except 2 and 4
+ *
+ * Return: nth, void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
+			_putchar(i);
+	_putchar('\n');
+}
