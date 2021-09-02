94bd272a372ab817409610fe2efe362fbe1063da
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 12:22:06 -0500

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 to stdout 10x
+ *
+ * Return: nth, void
+ */
+void more_numbers(void)
+{
+	int j, i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
