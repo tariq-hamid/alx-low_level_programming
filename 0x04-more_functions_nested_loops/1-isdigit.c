9f7a8df763aab8c5c87f93df9ba60552947ae7ee
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 09:49:32 -0500

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <ctype>
+
+/**
+ * _isdigit - checks if c is a digit (0 - 9)
+ * @c: passed to _isdigit
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (isdigit != 0)
+		return (1);
+	return (0);
+}
