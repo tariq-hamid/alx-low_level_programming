aad82616d5d403a94bf6ac7a5fb3831b405d6b5d
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 10:15:47 -0500

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0-14 to stout 10x
+ *
+ * Return: nth, void
+ */
+void more_numbers(void)
+{
+	int j, i;
+
+	for (j = 0; j < 10; j++)
+		for (i = 0; i <= 14; i++)
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		_putchar('\n');
+}
