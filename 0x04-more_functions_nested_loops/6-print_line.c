905ffa7811bcdf73b71fae742f1906efe5e6604d
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 10:33:03 -0500

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints a line
+ * @n: passed to print_line
+ *
+ * Return: nth, void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
