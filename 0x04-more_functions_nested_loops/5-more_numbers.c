d347de6459b5712f4b3b9c81d3e55c2cd3b417f9
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-02 10:20:44 -0500

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 	int j, i;
 
 	for (j = 0; j < 10; j++)
-		for (i = 0; i < 14; i++)
-			_putchar(i);
+		for (i = 0; i <= 14; i++)
+			_putchar(i + '0');
 		_putchar('\n');
 }
