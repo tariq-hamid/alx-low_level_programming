ca99bc164529b1195f69205c99c41b5b14ca8d24
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-07 03:51:38 -0500

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - prints every other element of a string
@@ -8,9 +9,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len;
 
-	while (str[i] != '\0')
+	len = strlen(str);
+
+	while (i <= len)
 	{
 		_putchar(str[i]);
 		i += 2;
