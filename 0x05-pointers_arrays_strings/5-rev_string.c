cc0566d691ab6c913b39facc892d7eac5c7f849b
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-07 03:41:36 -0500

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: passed to rev_string
+ *
+ * Return: nth, void
+ */
+void rev_string(char *s)
+{
+	int i = 0, len;
+
+	len = strlen(s) - 1;
+
+	while (length > i)
+	{
+		swap_char(s + len, s + i);
+		i++;
+		length--;
+	}
+}
+
+/**
+ * swap_char - swaps the values of two chars, x and y
+ * @x: passed to swap_char
+ * @y: passed to swap_char
+ *
+ * Return: nth, void
+ **/
+void swap_char(char *x, char *y)
+{
+	char tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
