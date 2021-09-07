ede897cf4bc0760e3fe1b7925d58d36d48ae5add
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-07 04:01:13 -0500

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the half of a string
+ * @str: passed to puts_half
+ *
+ * Return: nth, void
+ */
+void puts_half(char *str)
+{
+	int len, half, i;
+
+	len = strlen(str);
+	half = len / 2;
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
