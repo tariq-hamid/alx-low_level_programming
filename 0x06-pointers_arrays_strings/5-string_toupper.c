9f9db154cd30b26f3baefcc85b6e905b7191e33a
Tariq Hamid <tariqhamid416@gmail.com>
2021-09-09 13:23:34 -0500

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase letters
+ * @str: passed to string_toupper
+ *
+ * Return: pointer to the string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
