9af51260840e7ca3850018c1500778af7be1312d
Tariq Hamid <tariq86hamid@gmail.com>
2021-09-29 08:50:58 +0300

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed, passed to print_name
+ * @f: callback, passed to print_name
+ *
+ * Return: nth, void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		f(name);
+}
