348797dd1f028df21afc72b25077d561bf2c53c1
Tariq Hamid <tariq86hamid@gmail.com>
2021-09-30 22:14:40 +0300

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,7 +9,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 char *make_nil(char *s);
-int count_format(const char * const format);
+int count_type_format(const char * const format);
 void print_comma(int j, int x);
 
 #endif
