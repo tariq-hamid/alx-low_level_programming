30217e45828a80d33e04858a5d633aef63b8d052
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-07 20:08:39 +0300

    implemeted list_t struct

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct list_ - linked list type
+ * @str: string - string to be stored in the node
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
 #endif
