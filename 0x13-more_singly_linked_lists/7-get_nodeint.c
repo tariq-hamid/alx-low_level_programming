a9e483f4190d075dd9ebfdbef99c08ac89f27e4c
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-13 20:36:10 +0300

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node required
+ *
+ * Return: the address of the node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *nextNode = NULL;
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i <= index)
+		{
+			if (i == index)
+			{
+				nextNode = head;
+				break;
+			}
+			head = head->next;
+			i++;
+		}
+		else
+			return (NULL);
+	}
+	return (nextNode);
+}
