449a65d448c2097c1e2531616ae0686ce08d54c5
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-13 20:40:09 +0300

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the sum of all the data 
+ * get_nodeint_at_index - returns the sum of all the data
  * (n) of a listint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node required
