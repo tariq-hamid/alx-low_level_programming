96e446985d37c6abb7da0a0f760e92c5dfbbc0eb
Tariq Hamid <tariq86hamid@gmail.com>
2021-10-13 19:34:37 +0300

    added lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct listint_ - singly linked list structure
+ * @n: integer, to be stored
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_
+{
+	int n;
+	struct listint_ *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 
 #endif
